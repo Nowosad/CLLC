51f52592e1e7a10b02c73aefbdf52db8f23dc246
Nowosad <tupiszakaczki@gmail.com>
2019-04-19 14:56:03 +0200

    adds file compression

--- R/02-split_bands.R
+++ R/02-split_bands.R
@@ -7,8 +7,9 @@ band_splitter = function(year){
   names(years) = seq_along(years)
   my_id = as.numeric(names(years[years==year]))
   gdal_translate(src_dataset = "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif",
-                 dst_dataset = sprintf("raw_data/cci_lc_%s.tif", year),
-                 b = my_id)    
+                 dst_dataset = sprintf("data-raw/cci_lc_%s.tif", year),
+                 b = my_id,
+                 co = "COMPRESS=LZW")    
 }
 
 future_lapply(1992:2015, band_splitter)
