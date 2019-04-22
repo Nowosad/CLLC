66dc40ecf4aea85e99d14f6eea1c0ea4619985fc
nowosad <tupiszakaczki@gmail.com>
2019-04-22 04:45:09 -0400

    generalize extractions

--- R/04-extract_values.R
+++ R/04-extract_values.R
@@ -2,6 +2,7 @@ library(gdalUtils)
 library(raster)
 library(sf)
 
+# create a countries raster -----------------------------------------------
 lc = raster("data/cci_lc_1992.tif")
 countries = st_read("data-raw/gpw_v4_national_identifier_grid_rev11_30_sec.shp")
 countries$id = seq_len(nrow(countries))
@@ -19,11 +20,29 @@ gdal_rasterize(
   a_nodata = 0
 )
 
-system(sprintf("gpat_polygon -i %s -e %s -o %s -s %s -n %s -m %s -t %i",
-               "data/cci_lc_1992.tif",
-               "data/countries_raster.tif",
-               "data/cci_lc_1992_countries.csv",
-               "prod",
-               "none",
-               "200000",
-               1))
+
+# calculate the land cover composition for each year ----------------------
+# this step requires geopat2 and about 200GB of RAM -----------------------
+# https://github.com/Nowosad/geopat2 --------------------------------------
+
+lc_composite = function(input_raster){
+  output_raster = paste0(tools::file_path_sans_ext(input_raster), "_countries.csv")
+  system(
+    sprintf(
+      "gpat_polygon -i %s -e %s -o %s -s %s -n %s -m %s -t %i",
+      input_raster,
+      "data/countries_raster.tif",
+      output_raster,
+      "prod",
+      "none",
+      "200000",
+      1
+    )
+  ) 
+}
+
+lc_rasters = dir("data",
+                 pattern = "^cci_lc.*.tif$", 
+                 full.names = TRUE)
+
+lapply(lc_rasters, lc_composite)
