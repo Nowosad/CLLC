d12b9e46d4eba1f8b83532f5bb3f6d0635396080
nowosad <tupiszakaczki@gmail.com>
2019-04-27 14:35:08 -0400

    fixes format

--- R/02-reproject.R
+++ R/02-reproject.R
@@ -3,7 +3,6 @@ library(gdalUtils)
 # reproject equal area ----------------------------------------------------
 gdalwarp("data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif",
          "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.tif",
-         of = "VRT",
          r = "near", #alternative - mode?
          t_srs = "+proj=cea",  #https://proj4.org/operations/projections/cea.html,
          multi = TRUE,
