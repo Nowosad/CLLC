ba5c7d98eef2f5a6729d5377b7b6e227d7c3570c
nowosad <tupiszakaczki@gmail.com>
2019-04-27 14:31:17 -0400

    reprojection

---
+++ R/02-reproject.R
@@ -0,0 +1,15 @@
+library(gdalUtils)
+
+# reproject equal area ----------------------------------------------------
+gdalwarp("data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif",
+         "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.vrt",
+         of = "VRT",
+         r = "near", #alternative - mode?
+         t_srs = "+proj=cea",  #https://proj4.org/operations/projections/cea.html,
+         multi = TRUE,
+         wm = 200000)
+
+# compress the output file ------------------------------------------------
+gdal_translate(src_dataset = "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.vrt",
+               dst_dataset = "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.tif",
+               co = c("COMPRESS=LZW", "BIGTIFF=YES"))
