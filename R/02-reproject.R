f0e9c2e020928e4c267a757c567b568cc271e191
nowosad <tupiszakaczki@gmail.com>
2019-04-27 14:33:42 -0400

    fixes resolution issue

--- R/02-reproject.R
+++ R/02-reproject.R
@@ -8,4 +8,5 @@ gdalwarp("data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif",
          t_srs = "+proj=cea",  #https://proj4.org/operations/projections/cea.html,
          multi = TRUE,
          wm = 200000,
-         co = c("COMPRESS=LZW"))
+         co = c("COMPRESS=LZW"),
+         tr = c(300, 300))
