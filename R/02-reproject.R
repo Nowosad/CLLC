1896537d44b86b66b8146a80650f0ef16fe86cf9
nowosad <tupiszakaczki@gmail.com>
2019-04-27 14:31:56 -0400

    cleans code

--- R/02-reproject.R
+++ R/02-reproject.R
@@ -2,12 +2,13 @@ library(gdalUtils)
 
 # reproject equal area ----------------------------------------------------
 gdalwarp("data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif",
-         "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.vrt",
+         "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.tif",
          of = "VRT",
          r = "near", #alternative - mode?
          t_srs = "+proj=cea",  #https://proj4.org/operations/projections/cea.html,
          multi = TRUE,
-         wm = 200000)
+         wm = 200000,
+         co = c("COMPRESS=LZW"))
 
 # compress the output file ------------------------------------------------
 gdal_translate(src_dataset = "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.vrt",
