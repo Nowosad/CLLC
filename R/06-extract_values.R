a5274e8b0cc679db512f07c25656cccca56e4c53
nowosad <tupiszakaczki@gmail.com>
2019-04-28 12:56:47 -0400

    cleans extracct values

--- R/06-extract_values.R
+++ R/06-extract_values.R
@@ -1,26 +1,3 @@
-library(gdalUtils)
-library(raster)
-library(sf)
-
-# create a countries raster -----------------------------------------------
-lc = raster("data/cci_lc_1992.tif")
-countries = st_read("data-raw/gpw_v4_national_identifier_grid_rev11_30_sec.shp")
-countries$id = seq_len(nrow(countries))
-
-st_write(countries, "data/countries.gpkg", delete_dsn = TRUE)
-
-gdal_rasterize(
-  "data/countries.gpkg",
-  "data/countries_raster.tif",
-  a = "id",
-  co = "COMPRESS=LZW",
-  te = as.vector(extent(lc))[c(1, 3, 2, 4)],
-  ts = c(ncol(lc), nrow(lc)),
-  ot = "Byte",
-  a_nodata = 0
-)
-
-
 # calculate the land cover composition for each year ----------------------
 # this step requires geopat2 and about 200GB of RAM -----------------------
 # https://github.com/Nowosad/geopat2 --------------------------------------
@@ -45,4 +22,4 @@ lc_rasters = dir("data",
                  pattern = "^cci_lc.*.tif$", 
                  full.names = TRUE)
 
-lapply(lc_rasters, lc_composite)
+lapply(lc_rasters[[1]], lc_composite)
