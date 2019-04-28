d8b640f940595c39b64b08acf1c9cd1956eae1fa
nowosad <tupiszakaczki@gmail.com>
2019-04-28 08:07:01 -0400

    renames split

--- R/03-split_bands.R
+++ R/03-split_bands.R
@@ -1,12 +1,12 @@
 library(gdalUtils)
 library(future.apply)
-plan(multisession, workers = 12)
+plan(multisession, workers = 1)
 
 band_splitter = function(year){
   years = 1992:2015
   names(years) = seq_along(years)
   my_id = as.numeric(names(years[years==year]))
-  gdal_translate(src_dataset = "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif",
+  gdal_translate(src_dataset = "data-raw/scratch/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7_ea.tif",
                  dst_dataset = sprintf("data-raw/cci_lc_%s.tif", year),
                  b = my_id,
                  co = "COMPRESS=LZW")    
