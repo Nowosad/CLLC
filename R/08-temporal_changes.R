fb505fb07d5d973dab598624e739fdce51718bb7
nowosad <tupiszakaczki@gmail.com>
2019-04-29 11:31:01 -0400

    calculate changes

---
+++ R/08-temporal_changes.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(purrr)
+
+change = function(year1, year2){
+  df = readr::read_csv("data/07-summaries.csv", col_types = readr::cols())
+  country_names = filter(df, Year == year1)["NAME0"]
+  df1 = filter(df, Year == year1) %>% 
+    select(Agriculture:Water)
+  df2 = filter(df, Year == year2) %>% 
+    select(Agriculture:Water)
+  df3 = df2 - df1
+  country_names$Year_change = paste0(year1, "_", year2)
+  df3 = cbind(country_names, df3)
+  df3
+} 
+
+year1 = 1992:2010
+year2 = 1997:2015
+
+changes5 = map2_dfr(year1, year2, change)
+
+dir.create("data/database")
+changes5 %>% 
+  readr::write_csv("data/database/all_changes5.csv")
+
+dir.create("data/epi")
+changes5 %>%
+  select(NAME0, Year_change, Grassland, Wetland) %>% 
+  readr::write_csv("data/epi/changes5.csv")
