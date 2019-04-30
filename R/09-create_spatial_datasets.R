20d6f3bfda704a03372e7e75950c5fb9dbe2397c
nowosad <tupiszakaczki@gmail.com>
2019-04-30 06:42:58 -0400

    creates spatial datasets

---
+++ R/09-create_spatial_datasets.R
@@ -0,0 +1,29 @@
+library(sf)
+library(dplyr)
+library(readr)
+library(purrr)
+dir.create("data/spatial")
+sf = read_sf("data-raw/gpw_v4_national_identifier_grid_rev11_30_sec.shp") %>% 
+  select(ISOCODE, UNSDCODE, NAME0)
+
+# spatial summaries -------------------------------------------------------
+save_spatial_summaries = function(year, x, y){
+  x = filter(x, Year == year)
+  y = left_join(y, x, by = "NAME0")
+  st_write(y, paste0("data/spatial/nlcc_", year, ".gpkg"))
+}
+
+all_summaries = read_csv("data/database/all_summaries.csv")
+1992:2015 %>% map(save_spatial_summaries, all_summaries, sf)
+
+# spatial changes ---------------------------------------------------------
+save_spatial_changes = function(year_change, x, y){
+  x = filter(x, Year_change == year_change)
+  x = rename(x, Years_transitions = Year_change)
+  y = left_join(y, x, by = "NAME0")
+  st_write(y, paste0("data/spatial/nlct_", year_change, ".gpkg"))
+}
+
+all_changes = read_csv("data/database/all_changes5.csv")
+unique(all_changes$Year_change) %>% map(save_spatial_changes, all_changes, sf)
+
