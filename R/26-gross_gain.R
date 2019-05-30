0c8cd83918cdda392bdc6cf9b2b9e092ecab304b
nowosad <tupiszakaczki@gmail.com>
2019-05-30 09:50:27 -0400

    calculates gross gain

--- R/26-gross_gain.R
+++ R/26-gross_gain.R
@@ -1,9 +1,9 @@
+library(sf)
 library(dplyr)
 library(purrr)
-library(sf)
 to_km2 = function(x) x * 0.3 * 0.3
 
-lc_loss = function(lc_class, df){
+lc_gain = function(lc_class, df){
   lc_df = tibble::tribble(
     ~id, ~lcc,
     1, "Agriculture", 
@@ -19,18 +19,18 @@ lc_loss = function(lc_class, df){
   
   id = as.character(lc_df[lc_df$lcc == lc_class, "id", drop = TRUE])
   
-  gross_loss = df %>% 
-    dplyr::select(starts_with(id), -contains(paste0(id, id))) 
+  gross_gain = df %>% 
+    dplyr::select(ends_with(id), -contains(paste0(id, id))) 
 
   result = df %>%
     dplyr::select(cat, Year_change) %>% 
     mutate(Land_cover = lc_class) %>% 
-    bind_cols(gross_loss) %>% 
+    bind_cols(gross_gain) %>% 
     tidyr::gather(key = "id", value = "value", -cat, -Year_change, -Land_cover) %>% 
-    mutate(id = as.integer(stringr::str_sub(id, start = -1))) %>% 
+    mutate(id = as.integer(stringr::str_sub(id, start = -2, end = -2))) %>% 
     left_join(lc_df, by = "id") %>% 
-    dplyr::select(cat, Year_change, From = Land_cover, To = lcc, Loss = value) %>% 
-    mutate(Loss = to_km2(Loss))
+    dplyr::select(cat, Year_change, From = lcc, To = Land_cover, Gain = value) %>% 
+    mutate(Gain = to_km2(Gain))
     
   result
 }
@@ -43,18 +43,18 @@ countries = sf::st_read("data/countries.gpkg") %>%
   dplyr::select(NAME0, id)
 
 # calculations ------------------------------------------------------------
-gross_loss = c("Agriculture", "Forest", "Grassland", "Wetland", "Settlement", "Shrubland",
+gross_gain = c("Agriculture", "Forest", "Grassland", "Wetland", "Settlement", "Shrubland",
                       "Sparse_vegetation", "Bare_area", "Water") %>% 
-  map_dfr(lc_loss, df = df) %>% 
+  map_dfr(lc_gain, df = df) %>% 
   left_join(countries, by = c("cat" = "id")) %>%
-  select(NAME0, Year_change, From, To, Loss)
+  select(NAME0, Year_change, From, To, Gain)
 
-readr::write_csv(gross_loss, "data/database/gross_loss.csv")
+readr::write_csv(gross_gain, "data/database/gross_gain.csv")
 
 # creates dataset for epi -------------------------------------------------
-dir.create("data/epi")
-epi_gross_loss = gross_loss %>%
-  filter(From %in% c("Grassland", "Wetland"))
-
-readr::write_csv(epi_gross_loss, "data/epi/gross_loss.csv")
-writexl::write_xlsx(epi_gross_loss, "data/epi/gross_loss.xlsx")
+# dir.create("data/epi")
+# epi_gross_gain = gross_gain %>%
+#   filter(From %in% c("Grassland", "Wetland"))
+# 
+# readr::write_csv(epi_gross_gain, "data/epi/gross_gain.csv")
+# writexl::write_xlsx(epi_gross_gain, "data/epi/gross_gain.xlsx")
